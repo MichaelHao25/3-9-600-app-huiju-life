51259ae95e7a425f36ca8b135d20f81a44a73a43
chenglong <double.check.hao@gmail.com>
2018-03-10 22:48:00 +0800

    fix some issue

--- js/js.js
+++ js/js.js
@@ -11,6 +11,56 @@ $('.main .page-unit-set .select').on('click', '.item', function() {
 });
 
 
+var dDate = new Date;
+var nTime = dDate.getTime()
+var jDate = {
+	'day': dDate.getDate(),
+	'month': dDate.getMonth() + 1,
+	'yaer': dDate.getFullYear()
+}
+$('.main .page-sport .date-range .date').html(jDate.yaer + '-' + jDate.month + '-' + jDate.day)
+
+
+$('.main .page-sport .date-range').attr('data-time', nTime);
+$('.main .page-sport .date-range').on('click', '.left-control', function() {
+	var oldTime = parseInt($('.main .page-sport .date-range').attr('data-time'));
+	oldTime -= 24 * 60 * 60 * 1000;
+	var newTime = new Date;
+	newTime.setTime(oldTime)
+	var date = {
+		'day': newTime.getDate(),
+		'month': newTime.getMonth() + 1,
+		'yaer': newTime.getFullYear()
+	}
+	$('.main .page-sport .date-range .date').html(date.yaer + '-' + date.month + '-' + date.day)
+	$('.main .page-sport .date-range').attr('data-time', newTime.getTime());
+}).on('click', '.right-control', function() {
+	var oldTime = parseInt($('.main .page-sport .date-range').attr('data-time'));
+	oldTime += 24 * 60 * 60 * 1000;
+	var newTime = new Date;
+	newTime.setTime(oldTime)
+	var date = {
+		'day': newTime.getDate(),
+		'month': newTime.getMonth() + 1,
+		'yaer': newTime.getFullYear()
+	}
+	$('.main .page-sport .date-range .date').html(date.yaer + '-' + date.month + '-' + date.day)
+	$('.main .page-sport .date-range').attr('data-time', newTime.getTime());
+});
+
+var media = document.createElement('style')
+media.innerHTML = ".cp{cursor: pointer;}";
+document.getElementsByTagName('head')[0].appendChild(media);
+$('body *[href]').addClass('cp');
+$('body *[href]').on('click', function() {
+	if ($(this).attr('target')) {
+		window.open($(this).attr('href'));
+	} else {
+		window.location.href = $(this).attr('href');
+	}
+});
+
+
 var mySwiper = new Swiper(' .swiper .swiper-container', {
 	pagination: ' .swiper .swiper-pagination',
 	slidesPerView: 3,
